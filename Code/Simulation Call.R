6b021014e392c5daa121df8b0d6a6e3c6efd294e
Glen Martin (work) <glen.martin@manchester.ac.uk>
2019-11-21 16:50:38 +0000

    Added simulation with lower marginal outcome rate

--- Code/Simulation Call.R
+++ Code/Simulation Call.R
@@ -7,5 +7,16 @@
 simulationcall.fnc(iter = 100,
                    N = 5000, 
                    rho.vals = c(0, 0.2, 0.5, 0.75, 0.95),
+                   beta_1_true = c(-1.0, log(2), log(1.00)),
+                   beta_2_true = c(-1.5, log(1.00), log(3)),
                    filename = "SimulationResults",
+                   startingseed = 4651)
+
+
+simulationcall.fnc(iter = 100,
+                   N = 5000, 
+                   rho.vals = c(0, 0.2, 0.5, 0.75, 0.95),
+                   beta_1_true = c(-3.0, log(2), log(1.00)),
+                   beta_2_true = c(-3.5, log(1.00), log(3)),
+                   filename = "SimulationResults_LowerMarginalProportion",
                    startingseed = 4651)
