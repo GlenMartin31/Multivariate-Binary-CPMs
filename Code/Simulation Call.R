136890367b9ceaecf9a67180c60cf241167700da
Glen Martin (work) <glen.martin@manchester.ac.uk>
2019-12-10 07:49:15 +0000

    Change starting seed of sens. analysis

--- Code/Simulation Call.R
+++ Code/Simulation Call.R
@@ -6,7 +6,7 @@
 
 simulationcall.fnc(iter = 100,
                    N = 5000, 
-                   rho.vals = c(0, 0.2, 0.5, 0.75, 0.95),
+                   rho.vals = c(0.95, 0.75, 0.5, 0.25, 0),
                    beta_1_true = c(-1.0, log(2), log(1.00)),
                    beta_2_true = c(-1.5, log(1.00), log(3)),
                    filename = "SimulationResults",
@@ -15,8 +15,8 @@ simulationcall.fnc(iter = 100,
 
 simulationcall.fnc(iter = 100,
                    N = 5000, 
-                   rho.vals = c(0, 0.2, 0.5, 0.75, 0.95),
+                   rho.vals = c(0.95, 0.75, 0.5, 0.25, 0),
                    beta_1_true = c(-3.0, log(2), log(1.00)),
                    beta_2_true = c(-3.5, log(1.00), log(3)),
                    filename = "SimulationResults_LowerMarginalProportion",
-                   startingseed = 4651)
+                   startingseed = 9473)
